369d2cedcbda6342286fa31ff55ed212bc277290
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-10-21 23:18:56 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ClassBasedComponent from './components/class-based-component'
+import FunctionalComponent from './components/functional-component'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,10 +12,12 @@ function App() {
     <>
      <div>
       <h1>React JS Concepts</h1>
-      <ClassBasedComponent />
+      <FunctionalComponent />
+      {/* <ClassBasedComponent /> */}
      </div> 
     </>
   )
 }
 
 export default App
+// npm run dev  - to run server
