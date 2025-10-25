6fb11e96532afd77b35b2c23a7a7ff3dce3cff76
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-10-25 17:42:19 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ClassBasedComponent from './components/class-based-component'
-import FunctionalComponent from './components/functional-component'
-import ProductList from './components/products'
-const dummyProducts = ['product 1', 'product 2', 'product 3', 'product 4']; 
+
 
 
 
@@ -15,10 +9,7 @@ function App() {
   return (
     <>
      <div>
-      <h1>React JS Concepts</h1>
-      {/* <FunctionalComponent /> */}
-      <ClassBasedComponent />
-      {/* <ProductList name="sushmitha" age="24" listOfproduct={dummyProducts} /> */}
+      <h1>React JS Concepts</h1> 
      </div> 
     </>
   )
