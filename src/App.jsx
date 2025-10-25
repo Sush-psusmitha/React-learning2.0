21749996978b271890fb180aab3bcf3d26fe632f
Sush-psusmitha <sushsusmitha13@gmail.com>
2025-10-25 19:38:29 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,12 @@ function App() {
       <h1>React JS Concepts</h1> 
       {/* <LearnComponent/> */}
      {/* <Head/> */}
-     <Student name="Sushmitha" age={23} isMarried = {true} />
-   
+     <Student name="Sushmitha" age={23} isMarried={true}/>
+     <Student name="sam" age={93} isMarried={true}/>
     </>
   )
 }
 
 export default App
 // npm run dev  - to run server
-
-// props = parent component lo unna data ni child ki pass cheyadame props by destructing
+// props - parent component lo unna data ni child ki pass cheyadame props by destructing
